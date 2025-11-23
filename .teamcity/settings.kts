bff67c4d0167afc6abffc94b64b4921942f07e0f
radoslaw.panuszewski <radoslaw.panuszewski@allegro.com>
2025-11-23 22:44:13 +0100

    Set agent connectivity timeout to 1h

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -18,6 +18,8 @@ version = "2025.07"
 project {
     params {
         param("teamcity.buildQueue.restartBuildAttempts", "0")
+        param("teamcity.agent.dead.threshold.secs", "3600")
+        param("teamcity.agent.inactive.threshold.secs", "3600")
     }
 
     sequentialChain {
