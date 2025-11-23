2e935499e8b58d834bb56b8313f8e9aee4eb6c9c
radoslaw.panuszewski <radoslaw.panuszewski@allegro.com>
2025-11-23 12:37:25 +0100

    Use matrix

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -2,19 +2,13 @@ import jetbrains.buildServer.configs.kotlin.BuildType
 import jetbrains.buildServer.configs.kotlin.CompoundStage
 import jetbrains.buildServer.configs.kotlin.DslContext
 import jetbrains.buildServer.configs.kotlin.Project
-import jetbrains.buildServer.configs.kotlin.PublishMode
-import jetbrains.buildServer.configs.kotlin.RelativeId
-import jetbrains.buildServer.configs.kotlin.buildFeatures.parallelTests
 import jetbrains.buildServer.configs.kotlin.buildSteps.gradle
-import jetbrains.buildServer.configs.kotlin.buildSteps.kotlinScript
-import jetbrains.buildServer.configs.kotlin.buildSteps.script
 import jetbrains.buildServer.configs.kotlin.matrix
 import jetbrains.buildServer.configs.kotlin.project
 import jetbrains.buildServer.configs.kotlin.sequential
 import jetbrains.buildServer.configs.kotlin.triggers.vcs
 import jetbrains.buildServer.configs.kotlin.ui.add
 import jetbrains.buildServer.configs.kotlin.version
-import java.io.File
 
 version = "2025.07"
 
@@ -65,31 +59,16 @@ object JvmTests : BaseBuildType() {
         artifactRules = "+:**/build/test-results/**/TEST-*.xml => test-results.zip"
 
         steps {
-            script {
-                scriptContent = "unzip test-results.zip"
-            }
-
-            kotlinScript {
-                content = File("process-test-results.main.kts").readText()
-            }
-
-//            gradle {
-//                tasks = "jvmTest"
-//            }
-        }
-
-        dependencies {
-            artifacts(RelativeId("JvmTests")) {
-                buildRule = lastPinned()
-                artifactRules = "test-results.zip"
+            gradle {
+                tasks = "jvmTest"
             }
         }
 
-//        features {
-//            matrix {
-//                param("testBatch", (1..10).map(Int::toString).map(::value))
-//            }
-//        }
+        features {
+            matrix {
+                param("testBatch", (1..10).map(Int::toString).map(::value))
+            }
+        }
 
         triggers {
             vcs { }
